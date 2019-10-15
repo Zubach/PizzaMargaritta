61a594af83842dd53abfe33e152b6a1222a57d2e
Zubach <vanyakage@gmail.com>
2019-10-15 20:42:26 +0300

    Entity Framework connected | EFContext and other classes

---
+++ PizzaMargaritta/Entities/Pizza.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PizzaMargaritta.Entities
+{
+    [Table("tblPizzas")]
+    public class Pizza
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+
+        public string Image { get; set; }
+
+
+
+
+        public int OrderID { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
