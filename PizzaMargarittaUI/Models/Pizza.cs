56c11a1b320fb3c3461ea2279ab345cc96fdb17f
MorgaN <MorgaN@MORGAN->
2019-10-22 18:29:18 +0300

    Pizza add beta v1.1

---
+++ PizzaMargarittaUI/Models/Pizza.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMargarittaUI.Models
+{
+    public class Pizza
+    {
+
+    
+
+  
+            public string Name { get; set; }
+
+
+            public decimal Price { get; set; }
+
+
+            public string Description { get; set; }
+
+   
+
+            public string Image { get; set; }
+
+
+
+
+ 
+   
+    }
+}
