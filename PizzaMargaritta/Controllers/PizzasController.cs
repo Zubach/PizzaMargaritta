7e0a753d4f6e3d743c1130e3d3febaef8d0d636e
Zubach <vanyakage@gmail.com>
2019-10-24 18:53:27 +0300

    Add PizzasController | method GET

---
+++ PizzaMargaritta/Controllers/PizzasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PizzaMargaritta.Entities;
+
+namespace PizzaMargaritta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        EFContext _context;
+        public PizzasController(EFContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ContentResult GetAll()
+        {
+            var list = _context.Pizzas.ToList();
+            return Content(JsonConvert.SerializeObject(list));
+        }
+    }
+}
