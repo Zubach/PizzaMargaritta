fd568c2a68f53c35adf9fcf44f788433ca6068ba
Zubach <vanyakage@gmail.com>
2019-11-07 20:47:41 +0200

    AddPizzaPage added(Admin UI)

---
+++ PizzaMargarittaAdminUI/Pages/AddPizzaPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PizzaMargarittaAdminUI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaMargarittaAdminUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for AddPizzaPage.xaml
+    /// </summary>
+    public partial class AddPizzaPage : Page
+    {
+        AdminModel admin;
+        public AddPizzaPage()
+        {
+            InitializeComponent();
+        }
+
+        public AddPizzaPage(AdminModel admin)
+        {
+            InitializeComponent();
+            this.admin = admin;
+        }
+
+        public string ImageToBase64(string path)
+        {
+            using (System.Drawing.Image image = System.Drawing.Image.FromFile(path))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
+        }
+
+
+    }
+}
