d10eac5c44bca048a92fe17b2027f7685b2c22ca
Zubach <vanyakage@gmail.com>
2019-11-13 20:48:21 +0200

    Filter for Clients with bugs(API)

---
+++ PizzaMargarittaAdminUI/Pages/SearchUserPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using PizzaMargaritta.Models;
+using PizzaMargarittaAdminUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaMargarittaAdminUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for SearchUserPage.xaml
+    /// </summary>
+    public partial class SearchUserPage : Page
+    {
+        AdminModel admin;
+        List<UserModel> pizzas;
+        public SearchUserPage()
+        {
+            InitializeComponent();
+        }
+        public SearchUserPage(AdminModel admin)
+        {
+            InitializeComponent();
+            this.admin = admin;
+
+        }
+
+        private void GoBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
